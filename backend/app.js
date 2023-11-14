c5692ff14a41fee0af3072343acc9103309920bb
unknown <risaatin07@gmail.com>
2023-11-14 21:58:49 +0800

    cloudinary

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const products = require('./routes/product');
 const auth = require('./routes/auth');
+const order = require('./routes/order');
 
-app.use(express.json());
+app.use(cors())
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit: "50mb", extended: true }));
 app.use(cookieParser());
 
 app.use('/api/v1', products);
 app.use('/api/v1', auth);
 app.use('/api/v1', order);
+
+
 
 module.exports = app
