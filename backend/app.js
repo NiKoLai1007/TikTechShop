609ecea3f0989dc5b1dce323e7f0fa879844de25
ecapaglingayen@gmail.com <eumancarl.paglingayen@tup.edu.ph>
2023-11-27 01:17:25 +0800

    AddingToApp.js

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,8 @@ const cors = require('cors')
 const products = require('./routes/product');
 const auth = require('./routes/auth');
 const order = require('./routes/order');
-const category = require('./routes/category')
+const category = require('./routes/category');
+const brand = require('./routes/brand');
 
 app.use(cors())
 app.use(express.json({limit:'50mb'}));
@@ -16,6 +17,7 @@ app.use('/api/v1', products);
 app.use('/api/v1', auth);
 app.use('/api/v1', order);
 app.use('/api/v1', category);
+app.use('/api/v1', brand);
 
 
 
