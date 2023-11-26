95ffb3ac9b10c0a20c05e24017e0747010845541
unknown <risaatin07@gmail.com>
2023-11-26 23:29:42 +0800

    profile/order

--- frontend/src/Components/User/Register.js
+++ frontend/src/Components/User/Register.js
@@ -1,58 +1,39 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import Metadata from '../Layout/Metadata';
-import axios from 'axios';
+import React, { Fragment, useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Metadata from '../Layout/Metadata'
+import axios from 'axios'
 
 const Register = () => {
+
     const [user, setUser] = useState({
         name: '',
         email: '',
         password: '',
-    });
+    })
 
     const { name, email, password } = user;
-    const [avatar, setAvatar] = useState('');
-    const [avatarPreview, setAvatarPreview] = useState('/images/default_avatar.jpg');
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [errors, setErrors] = useState({});
-    const [loading, setLoading] = useState(true);
 
-    let navigate = useNavigate();
+    const [avatar, setAvatar] = useState('')
+    const [avatarPreview, setAvatarPreview] = useState('/images/default_avatar.jpg')
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(true)
+   
 
+    let navigate = useNavigate()
     useEffect(() => {
         if (isAuthenticated) {
-            navigate('/');
+            navigate('/')
         }
-    }, [isAuthenticated, navigate]);
+        if (error) {
+            console.log(error)
+           setError()
+        }
+
+    }, [error. isAuthenticated, navigate])
 
     const submitHandler = (e) => {
         e.preventDefault();
-
-        // Basic validation
-        const validationErrors = {};
-        if (!name.trim()) {
-            validationErrors.name = 'Name is required';
-        }
-        if (!email.trim()) {
-            validationErrors.email = 'Email is required';
-        }
-        if (!password.trim()) {
-            validationErrors.password = 'Password is required';
-        }
-
-        // Add avatar validation only if any field is filled
-        if (Object.values(user).some(value => value !== '')) {
-            if (!avatar) {
-                validationErrors.avatar = 'Avatar is required';
-            } else if (!avatar.type.startsWith('image/')) {
-                validationErrors.avatar = 'Please choose a valid image file';
-            }
-        }
-
-        if (Object.keys(validationErrors).length > 0) {
-            setErrors(validationErrors);
-            return;
-        }
 
         const formData = new FormData();
         formData.set('name', name);
@@ -60,65 +41,69 @@ const Register = () => {
         formData.set('password', password);
         formData.set('avatar', avatar);
 
-        register(formData);
-    };
+        register(formData)
+    }
 
-    const onChange = (e) => {
-        setErrors({ ...errors, [e.target.name]: '' });
-
+    const onChange = e => {
         if (e.target.name === 'avatar') {
+
             const reader = new FileReader();
 
             reader.onload = () => {
                 if (reader.readyState === 2) {
-                    setAvatarPreview(reader.result);
-                    setAvatar(reader.result);
+                    setAvatarPreview(reader.result)
+                    setAvatar(reader.result)
                 }
-            };
+            }
 
-            reader.readAsDataURL(e.target.files[0]);
+            reader.readAsDataURL(e.target.files[0])
+
         } else {
-            setUser({ ...user, [e.target.name]: e.target.value });
+            setUser({ ...user, [e.target.name]: e.target.value })
         }
-    };
+    }
 
     const register = async (userData) => {
         try {
             const config = {
                 headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            };
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
 
-            const { data } = await axios.post(`http://localhost:4001/api/v1/register`, userData, config);
-            console.log(data.user);
-            setIsAuthenticated(true);
-            setLoading(false);
-            setUser(data.user);
-            navigate('/');
+            const { data } = await axios.post(`http://localhost:4001/api/v1/register`, userData, config)
+            console.log(data.user)
+            setIsAuthenticated(true)
+            setLoading(false)
+            setUser(data.user)
+            navigate('/')
+
         } catch (error) {
-            setIsAuthenticated(false);
-            setLoading(false);
-            setUser(null);
-            setErrors({ ...errors, general: error.response.data.message });
-            console.log(error.response.data.message);
+            setIsAuthenticated(false)
+            setLoading(false)
+            setUser(null)
+            setError(error.response.data.message)
+            console.log(error.response.data.message)
         }
-    };
+    }
+
 
     return (
         <Fragment>
+
             <Metadata title={'Register User'} />
+
             <div className="row wrapper justify-content-center">
-                <div className="col-12 col-md-5">
+            <div className="col-12 col-md-5">
                     <img
                         src="images/loginpic2.png"
                         alt="Your Image"
                         className="img-fluid"
                         style={{ height: '450px' }}
                     />
-                </div>
+            </div>
                 <div className="col-10 col-lg-5">
-                    <form className="shadow-lg" onSubmit={submitHandler} encType="multipart/form-data">
+                    <form className="shadow-lg" onSubmit={submitHandler} encType='multipart/form-data'>
                         <h1 className="mb-3">Register</h1>
 
                         <div className="form-group">
@@ -126,12 +111,11 @@ const Register = () => {
                             <input
                                 type="name"
                                 id="name_field"
-                                className={`form-control ${errors.name ? 'is-invalid' : ''}`}
-                                name="name"
+                                className="form-control"
+                                name='name'
                                 value={name}
                                 onChange={onChange}
                             />
-                            {errors.name && <div className="invalid-feedback">{errors.name}</div>}
                         </div>
 
                         <div className="form-group">
@@ -139,12 +123,11 @@ const Register = () => {
                             <input
                                 type="email"
                                 id="email_field"
-                                className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-                                name="email"
+                                className="form-control"
+                                name='email'
                                 value={email}
                                 onChange={onChange}
                             />
-                            {errors.email && <div className="invalid-feedback">{errors.email}</div>}
                         </div>
 
                         <div className="form-group">
@@ -152,56 +135,55 @@ const Register = () => {
                             <input
                                 type="password"
                                 id="password_field"
-                                className={`form-control ${errors.password ? 'is-invalid' : ''}`}
-                                name="password"
+                                className="form-control"
+                                name='password'
                                 value={password}
                                 onChange={onChange}
                             />
-                            {errors.password && <div className="invalid-feedback">{errors.password}</div>}
                         </div>
 
-                        <div className="form-group">
-                            <label htmlFor="avatar_upload">Avatar</label>
-                            <div className="d-flex align-items-center">
+                        <div className='form-group'>
+                            <label htmlFor='avatar_upload'>Avatar</label>
+                            <div className='d-flex align-items-center'>
                                 <div>
-                                    <figure className="avatar mr-3 item-rtl">
+                                    <figure className='avatar mr-3 item-rtl'>
                                         <img
                                             src={avatarPreview}
-                                            className="rounded-circle"
-                                            alt="Avatar Preview"
+                                            className='rounded-circle'
+                                            alt='Avatar Preview'
                                         />
                                     </figure>
                                 </div>
-                                <div className="custom-file">
+                                <div className='custom-file'>
                                     <input
-                                        type="file"
-                                        name="avatar"
-                                        className={`custom-file-input ${errors.avatar ? 'is-invalid' : ''}`}
-                                        id="customFile"
-                                        accept="image/*"
+                                        type='file'
+                                        name='avatar'
+                                        className='custom-file-input'
+                                        id='customFile'
+                                        accept="images/*"
                                         onChange={onChange}
                                     />
-                                    <label className="custom-file-label" htmlFor="customFile">
+                                    <label className='custom-file-label' htmlFor='customFile'>
                                         Choose Avatar
                                     </label>
-                                    {errors.avatar && <div className="invalid-feedback">{errors.avatar}</div>}
                                 </div>
                             </div>
                         </div>
 
-                        <button id="register_button" type="submit" className="btn btn-block py-3">
+                        <button
+                            id="register_button"
+                            type="submit"
+                            className="btn btn-block py-3"
+                            // disabled={loading ? false : true}
+                        >
                             REGISTER
                         </button>
-                        <Link to="/login" className="float-right mt-3">
-                            Already Have an Account?
-                        </Link>
-
-                        {errors.general && <div className="text-danger mt-3">{errors.general}</div>}
                     </form>
                 </div>
             </div>
-        </Fragment>
-    );
-};
 
-export default Register;
+        </Fragment>
+    )
+}
+
+export default Register
