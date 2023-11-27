08d86462920e0c151978f2391a3f979ccdb8605a
Jurome <jaspherodevilas.09@gmail.com>
2023-11-27 15:46:48 +0800

    added-usercharts

---
+++ frontend/src/Components/Admin/UserCharts.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { ScatterChart, Scatter, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import axios from 'axios';
+import { getToken } from '../../utils/helpers';
+import Loader from '../Layout/Loader';
+
+const UserCharts = () => {
+  const [allUsers, setAllUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  const listUsers = async () => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${getToken()}`,
+        },
+      };
+
+      const { data } = await axios.get(`http://localhost:4001/api/v1/admin/users`, config);
+      setAllUsers(data.users);
+      setLoading(false);
+    } catch (error) {
+      setError(error.response?.data?.message || 'An error occurred');
+    }
+  };
+
+  useEffect(() => {
+    listUsers();
+  }, []);
+
+  return (
+    <ResponsiveContainer width="100%" height={1250}>
+      <ScatterChart data={allUsers}>
+        <CartesianGrid strokeDasharray="2 2" />
+        <XAxis dataKey="createdAt" />
+        <YAxis dataKey="name" type="category" />
+        <Tooltip />
+        <Legend />
+        <Scatter type="linear" dataKey="createdAt" fill="#8884d8" line={{ stroke: '#8884d8', strokeWidth: 2 }} />
+      </ScatterChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default UserCharts;
--- frontend/src/Components/Admin/UserSalesChart.js
+++ frontend/src/Components/Admin/UserSalesChart.js
@@ -1,47 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import { ScatterChart, Scatter, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import axios from 'axios';
-import { getToken } from '../../utils/helpers';
+import React, { useState, useEffect } from 'react'
+// import {
+//     BarChart,
+//     Bar,
+//     XAxis,
+//     YAxis,
+//     CartesianGrid,
+//     Tooltip,
+//     Legend,
+//     ResponsiveContainer,
+//     Cell,
+// } from "recharts";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { getToken } from "../../utils/helpers";
+import axios from "axios";
 import Loader from '../Layout/Loader';
 
-const UserCharts = () => {
-  const [allUsers, setAllUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
+const UserSalesChart = ({ data }) => {
+    const [sales, setSales] = useState('')
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState('')
+    const userSales = async () => {
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': `Bearer ${getToken()}`
+                }
+            }
 
-  const listUsers = async () => {
-    try {
-      const config = {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          Authorization: `Bearer ${getToken()}`,
-        },
-      };
-
-      const { data } = await axios.get(`http://localhost:4001/api/v1/admin/users`, config);
-      setAllUsers(data.users);
-      setLoading(false);
-    } catch (error) {
-      setError(error.response?.data?.message || 'An error occurred');
+            const { data } = await axios.get(`http://localhost:4001/api/v1/admin/customer-sales`, config)
+            setSales(data.customerSales)
+            setLoading(false)
+        } catch (error) {
+            setError(error.response.data.message)
+        }
     }
-  };
+    const barColors = ["#FFFFFF", "#FFFFFF", "#FFFFFF"]
+    useEffect(() => {
+        userSales()
+        // allUsers()
+    }, [])
 
-  useEffect(() => {
-    listUsers();
-  }, []);
+    return (
+        <ResponsiveContainer width="100%" height={300}>
+    {loading ? <Loader /> : (
+        <LineChart data={sales} >
+            <CartesianGrid strokeDasharray="2 2" />
+            <XAxis dataKey="userDetails.name" />
+            <YAxis tick={{ fill: '#ffffff' }} />
+            <Tooltip contentStyle={{ color: '#ffffff' }} />
+            <Legend />
 
-  return (
-    <ResponsiveContainer width="100%" height={1250}>
-      <ScatterChart data={allUsers}>
-        <CartesianGrid strokeDasharray="2 2" />
-        <XAxis dataKey="createdAt" />
-        <YAxis dataKey="name" type="category" />
-        <Tooltip />
-        <Legend />
-        <Scatter type="linear" dataKey="createdAt" fill="#8884d8" line={{ stroke: '#8884d8', strokeWidth: 2 }} />
-      </ScatterChart>
-    </ResponsiveContainer>
-  );
-};
+            <Line type="monotone" dataKey="total" stroke="#ffffff" strokeWidth={5} />
+        </LineChart>
+    )}
+</ResponsiveContainer>
 
-export default UserCharts;
+
+    );
+}
+export default UserSalesChart
