9fcdd6abdfe0d196fecc0e3ebc9a73bcd6f2f694
Jurome <jaspherodevilas.09@gmail.com>
2023-11-27 20:03:14 +0800

    added form validation new category

--- frontend/src/Components/Admin/newCategory.js
+++ frontend/src/Components/Admin/newCategory.js
@@ -8,142 +8,178 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const NewCategory = () => {
-    const [name, setName] = useState('');
-    const [images, setImages] = useState([]);
-    const [imagesPreview, setImagesPreview] = useState([]);
-    const [error, setError] = useState('');
-    const [loading, setLoading] = useState(true);
-    const [success, setSuccess] = useState('');
-    const [category, setCategory] = useState({});
+  const [name, setName] = useState('');
+  const [images, setImages] = useState([]);
+  const [imagesPreview, setImagesPreview] = useState([]);
+  const [nameError, setNameError] = useState('');
+  const [imagesError, setImagesError] = useState('');
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [success, setSuccess] = useState('');
+  const [category, setCategory] = useState({});
 
-    let navigate = useNavigate()
+  let navigate = useNavigate();
 
-    const submitHandler = (e) => {
-        e.preventDefault();
+  const validateForm = () => {
+    let isValid = true;
 
-        const formData = new FormData();
-        formData.set('name', name);
+    // Validate Name
+    if (!name.trim()) {
+      setNameError('Name is required');
+      isValid = false;
+    } else {
+      setNameError('');
+    }
 
-        images.forEach(image => {
-            formData.append('images', image)
-        })
+    // Validate Images
+    if (images.length === 0) {
+      setImagesError('Images are required');
+      isValid = false;
+    } else {
+      setImagesError('');
+    }
 
-        newCategory(formData);
-    };
+    return isValid;
+  };
 
-    const onChange = (e) => {
-        const files = Array.from(e.target.files);
-        setImagesPreview([]);
-        setImages([]);
-        
-        files.forEach(file => {
-            const reader = new FileReader();
-            reader.onload = () => {
-                if (reader.readyState === 2) {
-                    setImagesPreview(oldArray => [...oldArray, reader.result])
-                    setImages(oldArray => [...oldArray, reader.result])
-                }
-            }
-            
-            reader.readAsDataURL(file)
-            // console.log(reader)
-        })
-    };
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-    const newCategory = async (formData) => {
-        try {
-            const config = {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${getToken()}`
-                }
-            };
+    if (validateForm()) {
+      const formData = new FormData();
+      formData.set('name', name);
 
-            const { data } = await axios.post(`http://localhost:4001/api/v1/admin/category/new`, formData, config);
-            setLoading(false);
-            setSuccess(data.success);
-            setCategory(data.category);
-        } catch (error) {
-            setError(error.response.data.message);
+      images.forEach((image) => {
+        formData.append('images', image);
+      });
+
+      newCategory(formData);
+    }
+  };
+
+  const onChange = (e) => {
+    const files = Array.from(e.target.files);
+    setImagesPreview([]);
+    setImages([]);
+
+    files.forEach((file) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          setImagesPreview((oldArray) => [...oldArray, reader.result]);
+          setImages((oldArray) => [...oldArray, reader.result]);
         }
-    };
+      };
 
-    useEffect(() => {
-        if (error) {
-            toast.error(error, {
-                position: toast.POSITION.BOTTOM_RIGHT
-            });
-        }
+      reader.readAsDataURL(file);
+    });
+  };
 
-        if (success) {
-            navigate('/admin/Category');
-            toast.success('Category created successfully', {
-                position: toast.POSITION.BOTTOM_RIGHT
-            });
-        }
-    }, [error, success]);
+  const newCategory = async (formData) => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${getToken()}`,
+        },
+      };
 
-    return (
-        <Fragment>
-            <MetaData title={'New Category'} />
-            <div className="row">
-                <div className="col-12 col-md-2">
-                    <Sidebar />
+      const { data } = await axios.post(
+        `http://localhost:4001/api/v1/admin/category/new`,
+        formData,
+        config
+      );
+      setLoading(false);
+      setSuccess(data.success);
+      setCategory(data.category);
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error, {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    }
+
+    if (success) {
+      navigate('/admin/Category');
+      toast.success('Category created successfully', {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    }
+  }, [error, success]);
+
+  return (
+    <Fragment>
+      <MetaData title={'New Category'} />
+      <div className="row">
+        <div className="col-12 col-md-2">
+          <Sidebar />
+        </div>
+        <div className="col-12 col-md-10">
+          <Fragment>
+            <div className="wrapper my-5">
+              <form
+                className="shadow-lg"
+                onSubmit={submitHandler}
+                encType="multipart/form-data"
+              >
+                <h1 className="mb-4">New Category</h1>
+                <div className="form-group">
+                  <label htmlFor="name_field">Name</label>
+                  <input
+                    type="text"
+                    id="name_field"
+                    className={`form-control ${nameError ? 'is-invalid' : ''}`}
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  {nameError && <div className="invalid-feedback">{nameError}</div>}
                 </div>
-                <div className="col-12 col-md-10">
-                    <Fragment>
-                        <div className="wrapper my-5">
-                            <form className="shadow-lg" onSubmit={submitHandler} encType='multipart/form-data'>
-                                <h1 className="mb-4">New Category</h1>
-                                <div className="form-group">
-                                    <label htmlFor="name_field">Name</label>
-                                    <input
-                                        type="text"
-                                        id="name_field"
-                                        className="form-control"
-                                        value={name}
-                                        onChange={(e) => setName(e.target.value)}
-                                    />
-                                </div>
 
-                                <div className='form-group'>
-                                    <label>Images</label>
+                <div className="form-group">
+                  <label>Images</label>
 
-                                    <div className='custom-file'>
-                                        <input
-                                            type='file'
-                                            name='images'
-                                            className='custom-file-input'
-                                            id='customFile'
-                                            onChange={onChange}
-                                        multiple
-                                        />
-                                        <label className='custom-file-label' htmlFor='customFile'>
-                                            Choose Images
-                                        </label>
-                                    </div>
+                  <div className="custom-file">
+                    <input
+                      type="file"
+                      name="images"
+                      className={`custom-file-input ${imagesError ? 'is-invalid' : ''}`}
+                      id="customFile"
+                      onChange={onChange}
+                      multiple
+                    />
+                    <label className="custom-file-label" htmlFor="customFile">
+                      Choose Images
+                    </label>
+                  </div>
+                  {imagesError && <div className="invalid-feedback">{imagesError}</div>}
 
-                                    {imagesPreview.map(img => (
-                                        <img src={img} key={img} alt="Images Preview" className="mt-3 mr-2" width="55" height="52" />
-                                    ))}
-
-                                </div>
-
-                                <button
-                                    id="login_button"
-                                    type="submit"
-                                    className="btn btn-block py-3"
-                                >
-                                    CREATE
-                                </button>
-
-                            </form>
-                        </div>
-                    </Fragment>
+                  {imagesPreview.map((img) => (
+                    <img
+                      src={img}
+                      key={img}
+                      alt="Images Preview"
+                      className="mt-3 mr-2"
+                      width="55"
+                      height="52"
+                    />
+                  ))}
                 </div>
+
+                <button id="login_button" type="submit" className="btn btn-block py-3">
+                  CREATE
+                </button>
+              </form>
             </div>
-        </Fragment>
-    );
-}
+          </Fragment>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default NewCategory;
