7f4bd8ed22221f9c7fa547308c6d8fe2ce43f77e
ecapaglingayen@gmail.com <eumancarl.paglingayen@tup.edu.ph>
2023-11-27 15:31:58 +0800

    update monthlysales

--- frontend/src/Components/Admin/MonthlySalesChart.js
+++ frontend/src/Components/Admin/MonthlySalesChart.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
+// import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import { getToken } from "../../utils/helpers";
 import axios from "axios";
 import Loader from '../Layout/Loader';
+// import { BarChart } from 'recharts';
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 export default function MonthlySalesChart() {
     const [sales, setSales] = useState('')
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState('')
+    
     const monthlySales = async () => {
         try {
             const config = {
@@ -32,15 +35,15 @@ export default function MonthlySalesChart() {
 
     return (
         <ResponsiveContainer width="100%" height={300}>
-            {loading ? <Loader /> : ( <LineChart width={600} height={300} data={sales} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                <Line type="sans-serif" dataKey="total" stroke="#46948f" />
-                <CartesianGrid stroke="#4a5492" strokeDasharray="5 5" />
-                <XAxis dataKey="month" tick={{ fill: '#ffffff' }} />
-          <YAxis tick={{ fill: '#ffffff' }} />
-          <Tooltip contentStyle={{ color: '#ffffff' }} />
-            </LineChart> )}
+            {loading ? <Loader /> : (
+                <BarChart width={600} height={300} data={sales} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                    <Bar dataKey="total" fill="#7CB5EC" />
+                    <CartesianGrid stroke="#4a5492" strokeDasharray="5 5" />
+                    <XAxis dataKey="month" tick={{ fill: '#ffffff' }} />
+                    <YAxis tick={{ fill: '#ffffff' }} />
+                    <Tooltip contentStyle={{ color: '#ffffff' }} />
+                </BarChart>
+            )}
         </ResponsiveContainer>
-
-
     );
 }
