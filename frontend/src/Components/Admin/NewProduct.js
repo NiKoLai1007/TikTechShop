b65abc7737ab6726ca28eae0465ed6a8d98fa4b7
ecapaglingayen@gmail.com <eumancarl.paglingayen@tup.edu.ph>
2023-11-27 20:59:56 +0800

    Added Form Validation NewBrand.js

--- frontend/src/Components/Admin/NewProduct.js
+++ frontend/src/Components/Admin/NewProduct.js
@@ -1,58 +1,67 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Metadata from '../Layout/Metadata';
+import MetaData from '../Layout/Metadata';
 import Sidebar from './SideBar';
 import { getToken } from '../../utils/helpers';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const NewProduct = () => {
+const NewBrand = () => {
   const [name, setName] = useState('');
-  const [price, setPrice] = useState(0);
-  const [description, setDescription] = useState('');
-  const [category, setCategory] = useState('');
-  const [stock, setStock] = useState(0);
-  const [seller, setSeller] = useState('');
   const [images, setImages] = useState([]);
   const [imagesPreview, setImagesPreview] = useState([]);
+  const [nameError, setNameError] = useState('');
+  const [imagesError, setImagesError] = useState('');
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(true);
   const [success, setSuccess] = useState('');
-  const [product, setProduct] = useState({});
-
-  const categories = [
-    'Kids Watch',
-    'Mens Watch',
-    'Womens Watch',
-    'Sports Watch',
-    'Smart Watch',
-  ];
+  const [brand, setBrand] = useState({});
 
   let navigate = useNavigate();
+
+  const validateForm = () => {
+    let isValid = true;
+
+    // Validate Name
+    if (!name.trim()) {
+      setNameError('Name is required');
+      isValid = false;
+    } else {
+      setNameError('');
+    }
+
+    // Validate Images
+    if (images.length === 0) {
+      setImagesError('Images are required');
+      isValid = false;
+    } else {
+      setImagesError('');
+    }
+
+    return isValid;
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
 
-    const formData = new FormData();
-    formData.set('name', name);
-    formData.set('price', price);
-    formData.set('description', description);
-    formData.set('category', category);
-    formData.set('stock', stock);
-    formData.set('seller', seller);
+    if (validateForm()) {
+      const formData = new FormData();
+      formData.set('name', name);
 
-    images.forEach((image) => {
-      formData.append('images', image);
-    });
+      images.forEach((image) => {
+        formData.append('images', image);
+      });
 
-    newProduct(formData);
+      newBrand(formData);
+    }
   };
 
   const onChange = (e) => {
     const files = Array.from(e.target.files);
     setImagesPreview([]);
     setImages([]);
+
     files.forEach((file) => {
       const reader = new FileReader();
       reader.onload = () => {
@@ -66,7 +75,7 @@ const NewProduct = () => {
     });
   };
 
-  const newProduct = async (formData) => {
+  const newBrand = async (formData) => {
     try {
       const config = {
         headers: {
@@ -75,20 +84,16 @@ const NewProduct = () => {
         },
       };
 
-      console.log('Form Data:', formData); // Log form data before sending
-
       const { data } = await axios.post(
-        `http://localhost:4001/api/v1/admin/product/new`,
+        `http://localhost:4001/api/v1/admin/brand/new`,
         formData,
         config
       );
-
       setLoading(false);
       setSuccess(data.success);
-      setProduct(data.product);
+      setBrand(data.brand);
     } catch (error) {
-      console.error(error);
-      setError(error.response?.data?.message || 'Something went wrong');
+      setError(error.response.data.message);
     }
   };
 
@@ -100,125 +105,93 @@ const NewProduct = () => {
     }
 
     if (success) {
-      navigate('/admin/products');
-      toast.success('Product created successfully', {
+      navigate('/admin/brand');
+      toast.success('Brand created successfully', {
         position: toast.POSITION.BOTTOM_RIGHT,
       });
     }
-  }, [error, success, navigate]);
+  }, [error, success]);
 
   return (
     <Fragment>
-      <Metadata title={'New Product'} />
+      <MetaData title={'New Brand'} />
       <div className="row">
-                <div className="col-12 col-md-2">
-                    <Sidebar />
+        <div className="col-12 col-md-2">
+          <Sidebar />
+        </div>
+
+        <div className="col-12 col-md-10">
+          <Fragment>
+            <div className="wrapper my-5">
+              <form
+                className="shadow-lg"
+                onSubmit={submitHandler}
+                encType="multipart/form-data"
+              >
+                <h1 className="mb-4">New Brand</h1>
+
+                <div className="form-group">
+                  <label htmlFor="name_field">Name</label>
+                  <input
+                    type="text"
+                    id="name_field"
+                    className={`form-control ${nameError ? 'is-invalid' : ''}`}
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  {nameError && (
+                    <div className="invalid-feedback">{nameError}</div>
+                  )}
                 </div>
 
-                <div className="col-12 col-md-10">
-                    <Fragment>
-                        <div className="wrapper my-5">
-                            <form className="shadow-lg" onSubmit={submitHandler} encType='multipart/form-data'>
-                                <h1 className="mb-4">New Product</h1>
+                <div className="form-group">
+                  <label>Images</label>
 
-                                <div className="form-group">
-                                    <label htmlFor="name_field">Name</label>
-                                    <input
-                                        type="text"
-                                        id="name_field"
-                                        className="form-control"
-                                        value={name}
-                                        onChange={(e) => setName(e.target.value)}
-                                    />
-                                </div>
+                  <div className="custom-file">
+                    <input
+                      type="file"
+                      name="images"
+                      className={`custom-file-input ${
+                        imagesError ? 'is-invalid' : ''
+                      }`}
+                      id="customFile"
+                      onChange={onChange}
+                      multiple
+                    />
+                    <label className="custom-file-label" htmlFor="customFile">
+                      Choose Images
+                    </label>
+                  </div>
+                  {imagesError && (
+                    <div className="invalid-feedback">{imagesError}</div>
+                  )}
 
-                                <div className="form-group">
-                                    <label htmlFor="price_field">Price</label>
-                                    <input
-                                        type="text"
-                                        id="price_field"
-                                        className="form-control"
-                                        value={price}
-                                        onChange={(e) => setPrice(e.target.value)}
-                                    />
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor="description_field">Description</label>
-                                    <textarea className="form-control" id="description_field" rows="8" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor="category_field">Category</label>
-                                    <select className="form-control" id="category_field" value={category} onChange={(e) => setCategory(e.target.value)}>
-                                        {categories.map(category => (
-                                            <option key={category} value={category} >{category}</option>
-                                        ))}
-
-                                    </select>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="stock_field">Stock</label>
-                                    <input
-                                        type="number"
-                                        id="stock_field"
-                                        className="form-control"
-                                        value={stock}
-                                        onChange={(e) => setStock(e.target.value)}
-                                    />
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor="seller_field">Seller Name</label>
-                                    <input
-                                        type="text"
-                                        id="seller_field"
-                                        className="form-control"
-                                        value={seller}
-                                        onChange={(e) => setSeller(e.target.value)}
-                                    />
-                                </div>
-
-                                <div className='form-group'>
-                                    <label>Images</label>
-
-                                    <div className='custom-file'>
-                                        <input
-                                            type='file'
-                                            name='images'
-                                            className='custom-file-input'
-                                            id='customFile'
-                                            onChange={onChange}
-                                            multiple
-                                        />
-                                        <label className='custom-file-label' htmlFor='customFile'>
-                                            Choose Images
-                                        </label>
-                                    </div>
-
-                                    {imagesPreview.map(img => (
-                                        <img src={img} key={img} alt="Images Preview" className="mt-3 mr-2" width="55" height="52" />
-                                    ))}
-
-                                </div>
-
-
-                                <button
-                                    id="login_button"
-                                    type="submit"
-                                    className="btn btn-block py-3"
-                                // disabled={loading ? true : false}
-                                >
-                                    CREATE
-                                </button>
-
-                            </form>
-                        </div>
-                    </Fragment>
+                  {imagesPreview.map((img) => (
+                    <img
+                      src={img}
+                      key={img}
+                      alt="Images Preview"
+                      className="mt-3 mr-2"
+                      width="55"
+                      height="52"
+                    />
+                  ))}
                 </div>
+
+                <button
+                  id="login_button"
+                  type="submit"
+                  className="btn btn-block py-3"
+                >
+                  CREATE
+                </button>
+              </form>
             </div>
+          </Fragment>
+        </div>
+      </div>
     </Fragment>
   );
 };
 
-export default NewProduct;
+export default NewBrand;
