4307c2c71743246d05b73d20f92f733ba4902e45
Jurome <jaspherodevilas.09@gmail.com>
2023-11-28 11:42:15 +0800

    table design

--- frontend/src/Components/Admin/BrandList.js
+++ frontend/src/Components/Admin/BrandList.js
@@ -1,124 +1,119 @@
-import React, { Fragment, useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { MDBDataTable } from 'mdbreact'
+import React, { Fragment, useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { MDBDataTable } from 'mdbreact';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import MetaData from '../Layout/Metadata'
-import Loader from '../Layout/Loader'
-import Sidebar from './SideBar'
+import MetaData from '../Layout/Metadata';
+import Loader from '../Layout/Loader';
+import Sidebar from './SideBar';
 import axios from 'axios';
 import { getToken, successMsg, errMsg } from '../../utils/helpers';
 import Toast from '../Layout/Toast';
 
 const BrandList = () => {
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState('')
-    const [deleteError, setDeleteError] = useState('')
-    const [brand, setBrand] = useState([])
-    const [isDeleted, setIsDeleted] = useState('')
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+    const [deleteError, setDeleteError] = useState('');
+    const [brand, setBrand] = useState([]);
+    const [isDeleted, setIsDeleted] = useState('');
     let navigate = useNavigate();
-    
+
     const config = {
         headers: {
-            'Content-Type': 'application/json', 
+            'Content-Type': 'application/json',
             'Authorization': `Bearer ${getToken()}`
         }
-    }
+    };
 
     const listBrand = async () => {
         try {
             const config = {
                 headers: {
-                  Authorization: `Bearer ${getToken()}`,
+                    Authorization: `Bearer ${getToken()}`,
                 },
-              };
-            const { data } = await axios.get(`http://localhost:4001/api/v1/admin/brand`, config)
-            setBrand(data.brand)
-            setLoading(false)
-
-        // } catch (error) {
-        //     setError(error.response.data.message)
-             } catch (err) {
-                console.error('getBrand error:', err)
-            
+            };
+            const { data } = await axios.get(`http://localhost:4001/api/v1/admin/brand`, config);
+            setBrand(data.brand);
+            setLoading(false);
+        } catch (err) {
+            console.error('getBrand error:', err);
         }
-    }
+    };
 
     const deleteBrand = async (id) => {
         try {
-            const { data } = await axios.delete(`http://localhost:4001/api/v1/admin/brand/${id}`, config)
-            setIsDeleted(data.success)
-            setLoading(false)
-            
+            const { data } = await axios.delete(`http://localhost:4001/api/v1/admin/brand/${id}`, config);
+            setIsDeleted(data.success);
+            setLoading(false);
         } catch (error) {
-           setError(error.response.data.message)
-            
+            setError(error.response.data.message);
         }
-    }
+    };
 
     useEffect(() => {
         listBrand();
         if (error) {
             errMsg(error);
-            setError('')
+            setError('');
         }
         if (isDeleted) {
             successMsg('Brand deleted successfully');
             navigate('/admin/brand');
-
         }
-
-    }, [error, deleteError, isDeleted,])
-
+    }, [error, deleteError, isDeleted]);
 
     const deleteBrandHandler = (id) => {
-       deleteBrand(id)
-    }
-    
+        deleteBrand(id);
+    };
+
     const setBrands = () => {
         const data = {
             columns: [
                 {
                     label: 'Brand ID',
                     field: 'id',
-                    sort: 'asc'
+                    sort: 'asc',
                 },
                 {
                     label: 'Brand Name',
                     field: 'name',
-                    sort: 'asc'
+                    sort: 'asc',
                 },
                 {
                     label: 'Image',
                     field: 'image',
-                    sort: 'asc'
+                    sort: 'asc',
                 },
                 {
                     label: 'Actions',
                     field: 'actions',
                 },
             ],
-            rows: []
-        }
-        brand.forEach(brand => {
+            rows: [],
+        };
+        brand.forEach((brand) => {
             data.rows.push({
                 id: brand._id,
                 name: brand.name,
                 image: brand.images.url,
-                actions: <Fragment>
-                    <Link to={`/admin/brand/${brand._id}`} className="btn btn-primary py-1 px-2">
-                        <i className="fa fa-pencil"></i>
-                    </Link>
-                    <button className="btn btn-danger py-1 px-2 ml-2" onClick={() => deleteBrandHandler(brand._id)}>
-                        <i className="fa fa-trash"></i>
-                    </button>
-                </Fragment>
-            })
-        })
+                actions: (
+                    <Fragment>
+                        <Link to={`/admin/brand/${brand._id}`} className="btn btn-primary py-1 px-2">
+                            <i className="fa fa-pencil"></i>
+                        </Link>
+                        <button
+                            className="btn btn-danger py-1 px-2 ml-2"
+                            onClick={() => deleteBrandHandler(brand._id)}
+                        >
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </Fragment>
+                ),
+            });
+        });
         return data;
-    }
+    };
 
-    console.log(brand)
     return (
         <Fragment>
             <MetaData title={'All Brand'} />
@@ -129,23 +124,33 @@ const BrandList = () => {
                 <div className="col-12 col-md-10">
                     <Fragment>
                         <h1 className="my-5">All Brands</h1>
-                        <button className='procreate  ml-auto'>
-                        <Link to="/admin/brand/new" className="procreate"><i className="fa fa-plus"></i>Create a Brand</Link>
+                        <button className="procreate ml-auto">
+                            <Link to="/admin/brand/new" className="procreate">
+                                <i className="fa fa-plus"></i>Create a Brand
+                            </Link>
                         </button>
-                        {loading ? <Loader /> : (
+                        {loading ? (
+                            <Loader />
+                        ) : (
                             <MDBDataTable
                                 data={setBrands()}
                                 className="px-3"
                                 bordered
                                 striped
                                 hover
+                                noBottomColumns // hide the bottom pagination and search bar
+                                style={{
+                                    border: '2px solid #007BFF', // Set border color to blue
+                                    borderRadius: '8px',
+                                    backgroundColor: '#CEE4FD', // Set background color to light blue
+                                }}
                             />
                         )}
                     </Fragment>
                 </div>
             </div>
         </Fragment>
-    )
-}
+    );
+};
 
-export default BrandList
+export default BrandList;
--- frontend/src/Components/Admin/CategoryList.js
+++ frontend/src/Components/Admin/CategoryList.js
@@ -1,118 +1,115 @@
-import React, { Fragment, useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { MDBDataTable } from 'mdbreact'
+import React, { Fragment, useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { MDBDataTable } from 'mdbreact';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import MetaData from '../Layout/Metadata'
-import Loader from '../Layout/Loader'
-import Sidebar from './SideBar'
+import MetaData from '../Layout/Metadata';
+import Loader from '../Layout/Loader';
+import Sidebar from './SideBar';
 import axios from 'axios';
 import { getToken, successMsg, errMsg } from '../../utils/helpers';
 import Toast from '../Layout/Toast';
 
 const CategoryList = () => {
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState('')
-    const [deleteError, setDeleteError] = useState('')
-    const [category, setCategory] = useState([])
-    const [isDeleted, setIsDeleted] = useState('')
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+    const [deleteError, setDeleteError] = useState('');
+    const [category, setCategory] = useState([]);
+    const [isDeleted, setIsDeleted] = useState('');
     let navigate = useNavigate();
     const config = {
         headers: {
-            'Content-Type': 'application/json', 
+            'Content-Type': 'application/json',
             'Authorization': `Bearer ${getToken()}`
         }
-    }
+    };
 
     const listCategory = async () => {
         try {
             const config = {
                 headers: {
-                  Authorization: `Bearer ${getToken()}`,
+                    Authorization: `Bearer ${getToken()}`,
                 },
-              };
-            const { data } = await axios.get(`http://localhost:4001/api/v1/admin/category`, config)
-            setCategory(data.category)
-            setLoading(false)
-
+            };
+            const { data } = await axios.get(`http://localhost:4001/api/v1/admin/category`, config);
+            setCategory(data.category);
+            setLoading(false);
         } catch (error) {
-            setError(error.response.data.message)
-            
+            setError(error.response.data.message);
         }
-    }
+    };
 
     const deleteCategory = async (id) => {
         try {
-            const { data } = await axios.delete(`http://localhost:4001/api/v1/admin/category/${id}`, config)
-            setIsDeleted(data.success)
-            setLoading(false)
-            
+            const { data } = await axios.delete(`http://localhost:4001/api/v1/admin/category/${id}`, config);
+            setIsDeleted(data.success);
+            setLoading(false);
         } catch (error) {
-           setError(error.response.data.message)
-            
+            setError(error.response.data.message);
         }
-    }
+    };
 
     useEffect(() => {
         listCategory();
         if (error) {
             errMsg(error);
-            setError('')
+            setError('');
         }
         if (isDeleted) {
             successMsg('Category deleted successfully');
             navigate('/admin/category');
-
         }
-
-    }, [error, deleteError, isDeleted,])
-
+    }, [error, deleteError, isDeleted]);
 
     const deleteCategoryHandler = (id) => {
-       deleteCategory(id)
-    }
+        deleteCategory(id);
+    };
+
     const setCategories = () => {
         const data = {
             columns: [
                 {
-                   label: 'Category ID',
+                    label: 'Category ID',
                     field: 'id',
-                    sort: 'asc'
+                    sort: 'asc',
                 },
                 {
                     label: 'Category Name',
                     field: 'name',
-                    sort: 'asc'
+                    sort: 'asc',
                 },
                 {
                     label: 'Actions',
                     field: 'actions',
                 },
             ],
-            rows: []
-        }
-        category.forEach(category => {
+            rows: [],
+        };
+        category.forEach((category) => {
             data.rows.push({
                 id: category._id,
                 name: category.name,
-                image: category.images.url,
-                actions: <Fragment>
-                    <Link to={`/admin/category/${category._id}`} className="btn btn-primary py-1 px-2">
-                        <i className="fa fa-pencil"></i>
-                    </Link>
-                    <button className="btn btn-danger py-1 px-2 ml-2" onClick={() => deleteCategoryHandler(category._id)}>
-                        <i className="fa fa-trash"></i>
-                    </button>
-                </Fragment>
-            })
-        })
+                actions: (
+                    <Fragment>
+                        <Link to={`/admin/category/${category._id}`} className="btn btn-primary py-1 px-2">
+                            <i className="fa fa-pencil"></i>
+                        </Link>
+                        <button
+                            className="btn btn-danger py-1 px-2 ml-2"
+                            onClick={() => deleteCategoryHandler(category._id)}
+                        >
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </Fragment>
+                ),
+            });
+        });
         return data;
-    }
+    };
 
-    console.log(category)
     return (
         <Fragment>
-            <MetaData title={'All Users'} />
+            <MetaData title={'All Category'} />
             <div className="row">
                 <div className="col-12 col-md-2">
                     <Sidebar />
@@ -120,23 +117,33 @@ const CategoryList = () => {
                 <div className="col-12 col-md-10">
                     <Fragment>
                         <h1 className="my-4">All Category</h1>
-                        <button className='procreate  ml-auto'>
-                        <Link to="/admin/category/new" className="procreate"><i className="fa fa-plus"></i>Create a Category</Link>
+                        <button className="procreate ml-auto">
+                            <Link to="/admin/category/new" className="procreate">
+                                <i className="fa fa-plus"></i>Create a Category
+                            </Link>
                         </button>
-                        {loading ? <Loader /> : (
+                        {loading ? (
+                            <Loader />
+                        ) : (
                             <MDBDataTable
                                 data={setCategories()}
                                 className="px-3"
                                 bordered
                                 striped
                                 hover
+                                noBottomColumns // hide the bottom pagination and search bar
+                                style={{
+                                    border: '2px solid #007BFF', // Set border color to blue
+                                    borderRadius: '8px',
+                                    backgroundColor: '#CEE4FD', // Set background color to light blue
+                                }}
                             />
                         )}
                     </Fragment>
                 </div>
             </div>
         </Fragment>
-    )
-}
+    );
+};
 
-export default CategoryList
+export default CategoryList;
